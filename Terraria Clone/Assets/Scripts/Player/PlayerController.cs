64e011d0b308b1bb652a8152b0338c0674d3d74c
AUGIR <yourkraine@gmail.com>
2024-08-30 17:10:18 +0300

    Version 0.0.0.1.5
    Simple Player Movement Added

--- Terraria Clone/Assets/Scripts/Player/PlayerController.cs
+++ Terraria Clone/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    public float jumpForce;
+    public bool onGround;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D col)
     {
-        
+        if (col.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float jump = Input.GetAxisRaw("Jump");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        if (vertical > 0.1f || jump > 0.1f)
+        {
+            if (onGround)
+            {
+                movement.y = jumpForce;
+            }
+
+        }
+
+        rb.velocity = movement;
     }
 }
